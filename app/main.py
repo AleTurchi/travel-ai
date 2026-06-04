3e3e82e23e61a64a08c5437909f877fe17a56833
aleturchi7 <alessandro.turchiproximagroup.eu>
2026-06-04 15:40:02 +0200

    Add frontend Vercel URL to CORS

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://travel-ai-self.vercel.app",
+        "https://travel-ai-frontend-wheat.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
